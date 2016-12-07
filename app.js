1637b0e5cc627923ad33d54508e2f4b1ade96e40
mSorro <mxmwrk@gmail.com>
2016-12-07 20:43:42 +0300

    test desctop

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hello World! 3");
+var url = process.argv[2];
+console.log(url);
+
+let urli = {name : url};
+
+console.log(urli);
+
+console.log(`URL is ${url} `);
+console.log(`URL is ${urli.name}.`);
+
+
+
+let person = {name: 'John Smith'};   
+
+let tpl = `My name is ${person.name}.`;    
+
+let MyVar="My name is "+ person.name+".";
+
+
+console.log(`My name is ${person.name}.`);
+
+console.log(`This Works ${urli.name}.`);
+console.log('URL is ${urli.name}.');
+console.log('URL is ${urli.name} ');
+
+console.log("template literal= "+tpl);  
+
+console.log("my variable = "+MyVar);
